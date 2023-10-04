1f2e03eedcbeb73a92b116146427800d84907cea
Ankur-xavier <sarkar.ankur14@gmail.com>
2023-10-04 22:29:52 +0530

    leetcode easy

---
+++ src/com/coding/leetcode/SqaureRoot.java
@@ -0,0 +1,28 @@
+package com.coding.leetcode;
+//Q:https://leetcode.com/problems/sqrtx/description/
+
+public class SqaureRoot {
+
+	public static void main(String[] args) {
+	System.out.println(mySqrt(25));
+
+	}
+    public static  int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        int start = 1;
+        int end = x;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (Math.pow(mid, 2) == x) {
+                return mid;
+            } else if (Math.pow(mid, 2) < x) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end;
+    }
+}
